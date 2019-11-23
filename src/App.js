8f236cde8682395bbf2c19beae9a7b5a22fe7fb5
Tim Addison <git@tjaddison.com>
2019-11-23 11:53:48 -0500

    wire up all inputs

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ import "./styles/App.out.css";
 
 // https://stackoverflow.com/a/2117523
 function uuidv4() {
-  return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+  return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+    (
+      c ^
+      (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+    ).toString(16)
   );
 }
 
@@ -21,7 +24,7 @@ const schema = yup.object().shape({
   IsNotifyOnFailure: yup.string().required(),
   IsDeleted: yup.string().required(),
   IsEnabled: yup.string().required(),
-  NotifyLevelEventLog: yup.string().required(),
+  NotifyLevelEventLog: yup.string().required()
 });
 
 const TextInput = ({ name, placeholder }) => {
@@ -55,8 +58,8 @@ const GuidInput = ({ name, placeholder }) => {
       <button
         className="mt-2 bg-gray-200 text-gray-800 hover:bg-gray-400 py-2 px-4 rounded focus:outline-none focus:shadow-outline"
         onClick={e => {
-          e.preventDefault()
-          setFieldValue("TaskUid", uuidv4())
+          e.preventDefault();
+          setFieldValue("TaskUid", uuidv4());
         }}
       >
         Generate TaskUid
@@ -83,11 +86,15 @@ function App() {
           initialValues={{
             TaskUid: uuidv4(),
             Identifier: "",
-            TSQLCommand: ""
-          }}
-          validate={values => {
-            const errors = {};
-            return errors;
+            TSQLCommand: "",
+            StartTime: "00:00:00",
+            Frequency: "Day",
+            FrequencyInterval: "0",
+            NotifyOnFailureOperator: "",
+            IsNotifyOnFailure: "true",
+            IsDeleted: "false",
+            IsEnabled: "false",
+            NotifyLevelEventLog: "OnFailure"
           }}
           validationSchema={schema}
           onSubmit={(values, { setSubmitting }) => {
@@ -110,7 +117,10 @@ function App() {
               <TextInput name="StartTime" placeholder="Task name" />
               <TextInput name="Frequency" placeholder="Task name" />
               <TextInput name="FrequencyInterval" placeholder="Task name" />
-              <TextInput name="NotifyOnFailureOperator" placeholder="Task name" />
+              <TextInput
+                name="NotifyOnFailureOperator"
+                placeholder="Task name"
+              />
               <TextInput name="IsNotifyOnFailure" placeholder="Task name" />
               <TextInput name="IsDeleted" placeholder="Task name" />
               <TextInput name="IsEnabled" placeholder="Task name" />
@@ -130,7 +140,7 @@ function App() {
           <div className="mt-4">
             <textarea
               className="form-textarea block w-full mb-2"
-              rows={5}
+              rows={13}
               value={jsonTask}
               readOnly
             />
